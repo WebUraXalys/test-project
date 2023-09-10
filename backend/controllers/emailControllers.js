df828f7cdecc4dd0ca3a69439bcd6073bddc60ec
xalys_ura <xalysjyra1992@gmail.com>
2023-09-10 23:31:04 +0300

    receiving data on the backend

---
+++ backend/controllers/emailControllers.js
@@ -0,0 +1,21 @@
+const expressAsyncHandler = require("express-async-handler");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+dotenv.config();
+
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_MAIL, 
+    pass: process.env.SMTP_PASSWORD, 
+  },
+});
+
+const sendEmail = expressAsyncHandler(async (req, res) => {
+  const { email, name, message } = req.body;
+  console.log(email, name, message);
+});
+
+module.exports = { sendEmail };
